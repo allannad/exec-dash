43255284957c00727a0227324458a297c62d532d
allannad <65513146+allannad@users.noreply.github.com>
2020-06-06 18:53:25 -0400

    update with sales numbers

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -19,38 +19,62 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
-#prompt the user for a file
+#prompt the user for a file see https://www.youtube.com/watch?v=H71ts4XxWYU
 root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename()
 
-#validate the file and quit if does not meet condition
+#validate the file and quit if does not meet sales file regex condition
 print(file_path)
 validatethis = file_path[-16:]
 if re.match(r"^(sales)-[0-9][0-9][0-9][0-9][0-9][0-9].(csv)", validatethis):
-    print('Thank you for selecting the sales file')
+    print()
 else:
-    print('This is not a valid file')
+    print('This is not a valid file. Please select a monthly sales file.')
     exit()
 
+#create dataframe from selected file
+df = pd.read_csv(file_path, error_bad_lines=False)
 
+#create column for month and year of sales report
+df['year'] = pd.DatetimeIndex(df['date']).year
+df['month'] = pd.DatetimeIndex(df['date']).month
+df['monthname'] = pd.to_datetime(df['month'], format='%m').dt.month_name()
+#get month name as variable
+month = df.at[0,'monthname']
+#get year as variable
+year = df.at[0,'year']
+#calculate revenue
+revenue = df["sales price"].sum() 
+df["formattedrevenue"] = to_usd(revenue)
 
-
-
+#print beginning of report
 print("-----------------------")
-print("MONTH: March 2018")
-
+print("MONTH:" + ' ' + str(month) + ' ' + str(year))
 print("-----------------------")
 print("CRUNCHING THE DATA...")
-
 print("-----------------------")
-print("TOTAL MONTHLY SALES: $12,000.71")
+#print("TOTAL MONTHLY SALES: $12,000.71")
+print("TOTAL MONTHLY SALES:",to_usd(revenue))
 
+#identify Top selling products:
+#create new df of items with the max units sold
+pdsales = df.groupby(['product'], as_index=False).sum()
+pdsales["revenue"] = pdsales["sales price"]
+#make the column be in USD
+pdsales["formattedrevenue"] = pdsales["revenue"].apply(to_usd)
+
+#sort them by top sellers
+pdsalessorted = pdsales.sort_values(by=['revenue'], ascending=False)
+
+#add column with number of rows, to list out later. plus 1 is necessary as lists start at 0. :)
+pdsalessorted["number"] = numpy.arange(len(pdsalessorted)) + 1
+
+#iterate through and print the number in popularity, product name and total revenue
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
-print("  1) Button-Down Shirt: $6,960.35")
-print("  2) Super Soft Hoodie: $1,875.00")
-print("  3) etc.")
+for index, row in pdsalessorted.iterrows():
+    print(row['number'],row['product'],row['formattedrevenue'])
 
 print("-----------------------")
 print("VISUALIZING THE DATA...")
