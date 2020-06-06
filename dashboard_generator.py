20b1ad27931304a37b6d5349a35aa565713fbfc6
allannad <65513146+allannad@users.noreply.github.com>
2020-06-06 17:41:38 -0400

    select and validate sales file

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -1,6 +1,40 @@
 # dashboard_generator.py
 
 
+import pandas as pd
+import os
+import numpy
+import tkinter as tk
+from tkinter import filedialog
+import re
+
+
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+    Source: https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/datatypes/numbers.md#formatting-as-currency
+    Param: my_price (int or float) like 4000.444444
+    Example: to_usd(4000.444444)
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}" #> $12,000.71
+
+#prompt the user for a file
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
+
+#validate the file and quit if does not meet condition
+print(file_path)
+validatethis = file_path[-16:]
+if re.match(r"^(sales)-[0-9][0-9][0-9][0-9][0-9][0-9].(csv)", validatethis):
+    print('Thank you for selecting the sales file')
+else:
+    print('This is not a valid file')
+    exit()
+
+
+
 
 
 print("-----------------------")
