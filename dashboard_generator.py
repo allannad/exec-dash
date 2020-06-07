0182aa50f8f608a180f35c975ef25dc0aeaec65e
allannad <65513146+allannad@users.noreply.github.com>
2020-06-07 10:01:22 -0400

    create chart

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -7,7 +7,10 @@ import numpy
 import tkinter as tk
 from tkinter import filedialog
 import re
-
+import matplotlib as plot
+import matplotlib.pyplot as plt
+import altair as alt 
+from altair import Chart, X, Y, Axis, SortField
 
 def to_usd(my_price):
     """
@@ -25,7 +28,6 @@ root.withdraw()
 file_path = filedialog.askopenfilename()
 
 #validate the file and quit if does not meet sales file regex condition
-print(file_path)
 validatethis = file_path[-16:]
 if re.match(r"^(sales)-[0-9][0-9][0-9][0-9][0-9][0-9].(csv)", validatethis):
     print()
@@ -47,7 +49,7 @@ year = df.at[0,'year']
 #calculate revenue
 revenue = df["sales price"].sum() 
 df["formattedrevenue"] = to_usd(revenue)
-
+print(df)
 #print beginning of report
 print("-----------------------")
 print("MONTH:" + ' ' + str(month) + ' ' + str(year))
@@ -63,7 +65,7 @@ pdsales = df.groupby(['product'], as_index=False).sum()
 pdsales["revenue"] = pdsales["sales price"]
 #make the column be in USD
 pdsales["formattedrevenue"] = pdsales["revenue"].apply(to_usd)
-
+print(pdsales)
 #sort them by top sellers
 pdsalessorted = pdsales.sort_values(by=['revenue'], ascending=False)
 
@@ -76,6 +78,74 @@ print("TOP SELLING PRODUCTS:")
 for index, row in pdsalessorted.iterrows():
     print(row['number'],row['product'],row['formattedrevenue'])
 
+print(pdsalessorted)
+print(type(pdsalessorted))
+
+#DATA VISUALIZATION
+
 print("-----------------------")
 print("VISUALIZING THE DATA...")
 
+
+newdf = pdsalessorted.filter(['product','revenue','formattedrevenue'], axis=1)
+print(newdf)
+rev = newdf.at[0,'revenue']
+print(type(rev))
+
+
+
+productname = newdf['product'].tolist()
+revenueamt = newdf['revenue'].tolist()
+formattedrevenueamt = newdf['formattedrevenue'].tolist()
+#productname = []
+#revenueamt = []
+#for x in newdf:
+#    productname.append(x['product'])
+#    revenueamt.append(x['revenue'])
+print(productname)
+#print(revenueamt)
+print(formattedrevenueamt)
+
+
+x = [i for i in productname]
+rev = [i for i in revenueamt]
+
+x_pos = [i for i, _ in enumerate(x)]
+
+plt.barh(x_pos, rev, color='green')
+plt.ylabel("Product")
+plt.xlabel("Revenue ($)")
+plt.title("Top Selling Products" + ' ' + str(month) + ' ' + str(year))
+
+plt.yticks(x_pos, x)
+
+plt.show()
+
+
+
+
+#newdf.T.to_dict().values()
+#print(newdf)
+#newdf.to_dict('records')
+#print(records)
+#newdf.plot.bar()
+#newdf['formattedrevenue'].hist()
+
+#newdf.plot(kind="bar", x=pdsalessorted['product'], title="Sales",legend=False)
+
+
+#pdsalessorted.plot(kind="bar", x=pdsalessorted["product"], title="Sales",legend=False)
+
+#alt.Chart(pdsalessorted).mark_bar().encode(x='product',y='revenue')
+
+#pdsalessorted.plot.bar(x='product', y='revenue', title="Monthly sales by product")
+#plot.show(block=True)
+
+#bar_data = pdsalessorted
+#x = [i["product"] for i in bar_data]
+#viewers = [i["formattedrevenue"] for i in bar_data]
+#x_pos = [i for i, _ in enumerate(x)]
+#plt.barh(x_pos, viewers, color='green')
+
+
+#print(bar_data)
